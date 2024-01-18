dad9666145185c15614b6b636b62ff8d468a4a18
Eric Martindale <eric@ericmartindale.com>
2024-01-17 19:00:44 -0600

    Initial commit

---
+++ tests/hubs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Dependencies
+const assert = require('assert');
+const fetch = require('cross-fetch');
+const Fabric = require('@fabric/core');
+
+// Fabric Hubs
+const HUBS = [
+  'https://hub.fabric.pub'
+];
+
+describe('Fabric Hubs', function () {
+  describe('Hub', function () {
+    it('is a function', function () {
+      assert.strictEqual(Fabric instanceof Function, true);
+    });
+  });
+
+  describe('hub.fabric.pub', function () {
+    it('is accessible', async function () {
+      const result = await fetch(`https://hub.fabric.pub/`);
+      assert.strictEqual(result.status, 200);
+    });
+  });
+
+  describe('Sensemaker', function () {
+    it('is accessible', async function () {
+      const result = await fetch(`https://sensemaker.io/`);
+      assert.strictEqual(result.status, 200);
+    });
+  });
+});
